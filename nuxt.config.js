469fbf0c2f1789acf15378a339e5617b0f1e3d4f
michael garten <mjgartendev@gmail.com>
2018-04-25 15:14:46 -0500

    updated site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'personal-site',
+    title: 'Michael J. Garten',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
